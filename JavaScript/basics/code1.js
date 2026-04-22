f38c007b4c03f32da09787b748e1d32169276116
unknown <gdutt2304@gmail.com>
2026-04-22 13:33:42 +0530

    dataTypes

---
+++ JavaScript/basics/code1.js
@@ -0,0 +1,43 @@
+console.log("Code1.js ");
+
+a = 5;
+b = 6;
+c = a + b;
+console.log(c);
+
+// if  = 45❌
+// 1  = 67❌
+
+// String
+a = "String";
+console.log(a);
+
+// Symbol
+b = Symbol("Symbol");
+console.log(b);
+
+// Number
+num = 12;
+floatNum = 12.5;
+console.log(num, floatNum);
+
+// undefined
+b = undefined;
+console.log(b);
+
+// Null
+nullValue = null;
+console.log(nullValue);
+
+// Boolean
+a = true;
+b = false;
+console.log(a, b);
+
+// BigInt
+number = 98765456987654567898754345678907890n;
+// bigIntNumber = BigInt(number);
+// console.log(bigIntNumber);
+// console.log(number);
+
+// nsbu
