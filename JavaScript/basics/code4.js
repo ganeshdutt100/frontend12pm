da2e565e2957881b342672c74f925ae3f2fb3ee8
unknown <gdutt2304@gmail.com>
2026-04-23 12:59:26 +0530

    op

---
+++ JavaScript/basics/code4.js
@@ -0,0 +1,40 @@
+console.log("code4");
+
+// let a = 6;
+// let b = "6";
+
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a != b);
+
+//  && and
+// true && true = true
+// true && false = false
+// false && true = false
+// false && false = false
+// true || true = true
+// true || false = true
+// false || true = true
+// false || false = false
+
+// let a = true;
+// console.log(!a);
+
+// let a = 12;
+// let b = 6;
+
+// console.log(a > b && a >= b);
+// console.log(a > b || a == b);
+// console.log(!(a != b));
+
+let a = 5;
+a += 10;
+// a -= 10;
+// a *= 10;
+// a /= 10;
+// console.log(a);
+// a = a+10
