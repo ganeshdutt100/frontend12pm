f109d84d565b9b2668c01e930c3a287bda77219c
unknown <gdutt2304@gmail.com>
2026-04-27 12:51:47 +0530

    decision-making-stm

---
+++ JavaScript/Decision-making-stm/code1.js
@@ -0,0 +1,28 @@
+// syntax
+// if (condition) {
+// stm
+// }
+// let a = 90;
+
+// if (a > 40) {
+//   console.log(a);
+// }
+// if (a > 40) {
+//   console.log("a is greater than 40");
+// } else {
+//   console.log("a is not greater than 40");
+// }
+
+// marks = 59;
+
+// if (marks >= 90) {
+//   console.log("Grade A");
+// } else if (marks >= 80) {
+//   console.log("Grade B");
+// } else if (marks >= 70) {
+//   console.log("Grade C");
+// } else if (marks >= 60) {
+//   console.log("Grade D");
+// } else {
+//   console.log("fail");
+// }
