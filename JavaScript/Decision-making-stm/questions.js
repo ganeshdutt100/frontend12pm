e46fbff0a358c131117f7196015702e3aceb437c
unknown <gdutt2304@gmail.com>
2026-04-27 13:47:32 +0530

    switch statement

---
+++ JavaScript/Decision-making-stm/questions.js
@@ -0,0 +1,15 @@
+let year = 1900;
+
+if (year % 4 === 0) {
+  if (year % 100 === 0) {
+    if (year % 400 === 0) {
+      console.log(year + " is a leap year ");
+    } else {
+      console.log(year + " is not a leap year ");
+    }
+  } else {
+    console.log(year + " is a leap year ");
+  }
+} else {
+  console.log(year + " is not a leap year ");
+}
