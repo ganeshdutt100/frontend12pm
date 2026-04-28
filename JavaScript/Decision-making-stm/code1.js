3126a7522de0f6ae42f84214f19e538cc7d5add8
unknown <gdutt2304@gmail.com>
2026-04-28 12:59:53 +0530

    function

--- JavaScript/Decision-making-stm/code1.js
+++ JavaScript/Decision-making-stm/code1.js
@@ -52,31 +52,30 @@
 //   console.log("user not found");
 // }
 
-let day = 4;
+// let day = 4;
 
-switch (day) {
-  case 1:
-    console.log("monday");
-    break;
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
 
-  case 2:
-    console.log("tuesday");
-    break;
-  case 3:
-    console.log("wednesday");
-    break;
-  case 4:
-    console.log("thursday");
-    break;
-  case 5:
-    console.log("friday");
-    break;
-  case 6:
-    console.log("saturday");
-    break;
-  case 7:
-    console.log("sunday");
-    break;
-  default:
-    console.log("invalid day");
-}
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   case 7:
+//     console.log("sunday");
+//   default:
+//     console.log("invalid day");
+// }
---
+++ JavaScript/function/code1.js
@@ -0,0 +1,32 @@
+// function fun() {
+//   console.log("Hello World");
+// }
+// fun();
+
+// function fun(a, b) {
+//   return a + b;
+// }
+// let result = fun(12, 56);
+// console.log(result);
+
+// arrow function
+
+// let sum = (a, b) => {
+//   return console.log(a + b);
+// };
+// sum(12, 98);
+// let fun = () => {
+//   console.log("Hello World");
+//   console.log("Arrow function ");
+// };
+// fun();
+
+let fun = function (a, b) {
+  console.log("hello");
+};
+fun();
+
+// IIFE
+(function () {
+  console.log("hello world");
+})();
