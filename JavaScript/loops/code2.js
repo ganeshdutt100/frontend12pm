6dd1e3290e51f86c8b7a81b63af5b165504d7ecb
unknown <gdutt2304@gmail.com>
2026-05-04 13:44:10 +0530

    loop Questioins

--- JavaScript/loops/code2.js
+++ JavaScript/loops/code2.js
@@ -1,5 +1,28 @@
-let i = 0;
-while (i < 5) {
-  console.log(i);
-  i++;
+// let i = 0;
+// while (i < 5) {
+//   console.log(i);
+//   i++;
+// }
+let a = 12;
+let count = 0;
+while (a > 0) {
+  count++;
+  a = Math.floor(a / 10);
 }
+console.log(count);
+
+// let sum = 0;
+// while (a > 0) {
+//   let digit = a % 10;
+//   sum = sum + digit;
+//   a = Math.floor(a / 10);
+// }
+// console.log(sum);
+// let a = 1234;
+// let rev = 0;
+// while (a > 0) {
+//   let digit = a % 10;
+//   rev = rev * 10 + digit;
+//   a = Math.floor(a / 10);
+// }
+// console.log(rev);
