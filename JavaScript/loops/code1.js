ef4612f695f215540359e51fbbfa8cfaa04251b2
unknown <gdutt2304@gmail.com>
2026-05-04 12:52:57 +0530

    for loop

---
+++ JavaScript/loops/code1.js
@@ -0,0 +1,17 @@
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i, " :  Ganesh Dutt");
+// }
+let table = 12;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${table} * ${i} =  ${i * table}`);
+}
+// 2 *1 = 2
+// 2 *2 = 4
+// 2*3 = 6
+
+// let a = "Ganesh Dutt";
+// console.log("Hello :  ", a);
+// console.log(`hello :  ${a}`);
