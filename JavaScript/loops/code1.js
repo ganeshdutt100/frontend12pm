afc28039fbf07f09b8dd94ea3e4481472a43b006
unknown <gdutt2304@gmail.com>
2026-05-04 13:26:45 +0530

    for loop and while

--- JavaScript/loops/code1.js
+++ JavaScript/loops/code1.js
@@ -4,10 +4,10 @@
 // for (let i = 0; i <= 10; i++) {
 //   console.log(i, " :  Ganesh Dutt");
 // }
-let table = 12;
-for (let i = 1; i <= 10; i++) {
-  console.log(`${table} * ${i} =  ${i * table}`);
-}
+// let table = 12;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${table} * ${i} =  ${i * table}`);
+// }
 // 2 *1 = 2
 // 2 *2 = 4
 // 2*3 = 6
@@ -15,3 +15,35 @@ for (let i = 1; i <= 10; i++) {
 // let a = "Ganesh Dutt";
 // console.log("Hello :  ", a);
 // console.log(`hello :  ${a}`);
+
+// let n = 5;
+// let fact = 1;
+// for (let i = 1; i <= n; i++) {
+//   fact = fact * i;
+// }
+// console.log(fact);
+
+// let num = 12345;
+// let rev = 0;
+
+// for (let temp = num; temp > 0; temp = Math.floor(temp / 10)) {
+//   let digit = temp % 10;
+//   rev = rev * 10 + digit;
+// }
+// console.log(rev);
+
+// num = 12345
+// rcv = 0
+// temp = 12345
+// // 1
+// temp = 12345
+// digit  = 12345 % 10  = 5
+// rev =  0 *10 + 5 =  5
+// // 2
+// temp = 1234
+// digit  = 1234 % 10  = 4
+// rev =  5 *10 + 4 =  54
+// // 3
+// temp = 123
+// digit  = 123 % 10  = 3
+// rev =  54 *10 + 3 =  543
