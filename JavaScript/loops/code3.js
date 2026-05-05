0914d31df2f952aa3f329b9483db3fe18d839049
unknown <gdutt2304@gmail.com>
2026-05-05 12:51:26 +0530

    in and of loop

---
+++ JavaScript/loops/code3.js
@@ -0,0 +1,25 @@
+console.log("code 3");
+let i = 15;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
+let arr = [12, 34, 56, 78, 98];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let x of arr) {
+//   console.log(x);
+// }
+// let person = {
+//   name: "Amit",
+//   age: 25,
+//   address: "123 Main St",
+// };
+
+// for (let x in person) {
+//   console.log(x, person[x]);
+// }
