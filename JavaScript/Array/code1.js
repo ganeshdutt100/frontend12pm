3b7cde94ba2bfc5fa20e8891b820054ed82013d7
unknown <gdutt2304@gmail.com>
2026-05-07 13:23:36 +0530

    Array methods

---
+++ JavaScript/Array/code1.js
@@ -0,0 +1,46 @@
+// let arr = ["apple", "banana", "mango"];
+// let arrMixed = ["apple", 123, true, ["a", "b", "c"]];
+// let emptyArr = [];
+// emptyArr[0] = "empt1";
+// emptyArr[1] = "empt2";
+// arr[2] = "orange";
+// console.log(arr);
+// console.log(arrMixed);
+// console.log(emptyArr);
+
+// let arr = new Array("orange", "apple", "banana");
+
+// console.log(arr);
+
+// let arr = new Array();
+// arr[0] = "orange";
+// arr[1] = "apple";
+// arr[2] = "banana";
+
+// console.log(arr);
+
+// methods
+
+// mutator
+let arr = ["grapes", "kiwi", "apple", "kiwi", "banana", "mango", "kiwi"];
+// let numArr = [12, 34, 56, 78, 98];
+// let mixedArr = arr.concat(numArr);
+// let result = arr.includes("kiwi");
+// let result = arr.indexOf("kiwi", 2);
+// let result = arr.indexOf("kiwi");
+// let result = arr.lastIndexOf("kiwi");
+// let result = arr.join("/");
+// let result = arr.slice(1, 5);
+let result = arr.toString();
+
+console.log(result);
+// fill(value, start , end(n-1) )
+// arr.fill("javaScript", 1, 3);
+// arr.pop();
+// arr.push("js", "java");
+// arr.shift();
+// arr.unshift("js");
+// arr.sort();
+// arr.splice(1, 1, "js", "java");
+
+// console.log(arr);
