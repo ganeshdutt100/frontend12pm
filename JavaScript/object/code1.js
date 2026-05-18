b94627b1990f674f962e3aff31af250051eb4b1d
unknown <gdutt2304@gmail.com>
2026-05-18 13:00:43 +0530

    object values

--- JavaScript/object/code1.js
+++ JavaScript/object/code1.js
@@ -2,14 +2,14 @@
 // let age = "23";
 // let course = "fullstack ";
 
-let person = {
-  // key  :  values
-  name: "Divya",
-  age: 23,
-  course: "Frontend",
-  address: "Delhi",
-  number: 876545678,
-};
+// let person = {
+//   // key  :  values
+//   name: "Divya",
+//   age: 23,
+//   course: "Frontend",
+//   address: "Delhi",
+//   number: 876545678,
+// };
 
 // console.log(person.name);
 // console.log(person.age);
@@ -17,6 +17,21 @@ let person = {
 // console.log(person.course);
 // console.log(person.address);
 
-for (i in person) {
-  console.log(`${i} : ${person[i]}`);
-}
+// for (i in person) {
+//   console.log(`${i} : ${person[i]}`);
+// }
+
+// const person = {
+//   name: "priya",
+//   " home town": "delhi",
+//   age: 21,
+// };
+// delete person.age;
+
+// console.log(person.name);
+// console.log(person[" home town"]);
+
+// person.name = "Vaibhav";
+// console.log(person.name);
+
+// console.log(person);
