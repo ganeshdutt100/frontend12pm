62ab8e6b86c6723fe0581d4e6417016f39e7cdca
unknown <gdutt2304@gmail.com>
2026-05-18 12:50:45 +0530

    object

---
+++ JavaScript/object/code1.js
@@ -0,0 +1,22 @@
+// let personName = "Aqsa";
+// let age = "23";
+// let course = "fullstack ";
+
+let person = {
+  // key  :  values
+  name: "Divya",
+  age: 23,
+  course: "Frontend",
+  address: "Delhi",
+  number: 876545678,
+};
+
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.age);
+// console.log(person.course);
+// console.log(person.address);
+
+for (i in person) {
+  console.log(`${i} : ${person[i]}`);
+}
