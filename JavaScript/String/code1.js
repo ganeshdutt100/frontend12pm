1e15c1f76f70af3af48ac0b7e4d4edb890a0a7a8
unknown <gdutt2304@gmail.com>
2026-05-21 12:44:38 +0530

    String

---
+++ JavaScript/String/code1.js
@@ -0,0 +1,26 @@
+// name = "4achievers";
+// const singleQuoteString = "Hello, World!" + " : " + name;
+// const doubleQuoteString = "Hello, World!" + " : " + name;
+// const backtickString = `Hello, World! :  ${name}`;
+
+// console.log(singleQuoteString); // Output: Hello, World!
+// console.log(doubleQuoteString); // Output: Hello, World!
+// console.log(backtickString); // Output: Hello, World!
+
+const str = "javaScript is awesome";
+// console.log(str.length); // Output: 17
+// console.log(str.toUpperCase()); // Output: JAVASCRIPT IS AWESOME
+// console.log(str.toLowerCase()); // Output: javascript is awesome
+
+// console.log(str.includes("is")); // Output: true
+// console.log(str.includes("Java")); // Output: false
+// console.log(str.includes("Python")); // Output: false
+// console.log(str.slice(0, 4));
+
+const dirtyEmail = " dirtyemail@gmail.com ";
+console.log(dirtyEmail.trim()); // Output: "
+
+const message = "Hello Students, welcome to 4achievers";
+// console.log(message.split(" ")); // Output: ["Hello", "Students,", "welcome", "to", "4achievers"]
+console.log(message.replace("4achievers", "JavaScript")); // Output: Hello Students, welcome to JavaScript
+console.log(message.replaceAll("o", "a")); // Output: Hella Students, welacome to JavaScript
