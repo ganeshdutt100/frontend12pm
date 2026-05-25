d4cef9f9e380e1d6ced0d60708b81914b4768b11
unknown <gdutt2304@gmail.com>
2026-05-25 12:59:46 +0530

    BOM

---
+++ JavaScript/BOM/code1.js
@@ -0,0 +1,42 @@
+// console.log("BOM");
+// const arrFun = () => alert("Hello, World!");
+// const arrFun = () => confirm("Are you sure you want to proceed?");
+
+// let result = confirm("Are you sure you want to proceed?");
+
+// if (result) {
+//   console.log("User confirmed to proceed.");
+// } else {
+//   console.log("User canceled the operation.");
+// }
+
+// let userValue1 = Number(prompt("Please enter your age:"));
+// let userValue2 = Number(prompt("Please enter your age:"));
+// console.log(userValue1 + userValue2);
+
+// let setValue = setTimeout(() => {
+//   console.log("This message is displayed after 2 seconds.");
+// }, 2000);
+
+// clearTimeout(setValue);
+
+// let intervalId = setInterval(() => {
+//   console.log("This message is displayed every 3 seconds.");
+// }, 3000);
+
+// clearInterval(intervalId);
+
+// let arrFun = () => {
+//   close();
+// };
+// let arrFun = () => {
+//   print();
+// };
+
+// setTimeout(() => {
+//   close();
+// }, 2000);
+
+// let arrFun = () => {
+//   open("https://www.4achievers.com/", "_blank");
+// };
